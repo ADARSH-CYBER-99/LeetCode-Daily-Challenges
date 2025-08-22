b30a721c932e79b74cad13830eb381371ddd3b1d
ADARSH PRATAP SINGH <144412601+ADARSH-CYBER-99@users.noreply.github.com>
2025-08-22 09:28:56 +0530

    Update and rename FindtheMinimumAreatoCoverAllOnesI.java to Date_22_08_2025_FindtheMinimumAreatoCoverAllOnesI.java

---
+++ Date_22_08_2025_FindtheMinimumAreatoCoverAllOnesI.java
@@ -0,0 +1,28 @@
+// Date : 22-08-2025
+class Solution {
+    public int minimumArea(int[][] grid) {
+        int minRow = Integer.MAX_VALUE;
+        int maxRow = Integer.MIN_VALUE;
+        int minCol = Integer.MAX_VALUE;
+        int maxCol = Integer.MIN_VALUE;
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    minRow = Math.min(minRow, i);
+                    maxRow = Math.max(maxRow, i);
+                    minCol = Math.min(minCol, j);
+                    maxCol = Math.max(maxCol, j);
+                }
+            }
+        }
+
+        int height = maxRow - minRow + 1;
+        int width = maxCol - minCol + 1;
+
+        return height * width;
+    }
+}
